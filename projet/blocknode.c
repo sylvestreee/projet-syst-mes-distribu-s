bde89c10016a3257e431a534cac6ba7983c24f5a
MorganeRitter <morganeritter@hotmail.fr>
2018-05-12 20:10:33 +0200

    test

--- projet/blocknode.c
+++ projet/blocknode.c
@@ -1,63 +1,88 @@
-
-/** Peut se connecter à d'autres blockode à travers le serveur principale **/
-/*
-
-struct block {
-	int depth;
-	string creator;
-	string hash;
-	string waiting_requests[]; //struct -> ?
-};
-
-*/
-
-#include <stdio.h>
 #include <rpc/types.h>
 #include <rpc/xdr.h>
 #include <rpc/rpc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
-
-#define PROGNUM 0x20000100
+#define PROGNUM 0x20000135
 #define VERSNUM 1
 #define PROCNUM 1
 
+/* on utilise les thread pour lancer à la fois un server et un client pour un seul noeud*/
 
-
-int main (int argc, char **argv)
+int * hello(void)
 {
-  int res = 0,  n=0x41424344 ; 
-  char *host ;
-  enum clnt_stat stat ;
+    static int r = 0;
+    printf("Hello world\n");
+    fflush(stdout);
+    return &r;
+}
 
-  if (argc != 2)
-  {
-    printf("Usage: %s server_location\n",argv[0]) ;
-    exit(0) ;
-  }
-  
-  host = argv[1] ;
+void *node(void *arg)
+{
+    printf("launching server\n");
 
-  printf("client: variable n (begin) : %d %s,\n",n,(char *)&n) ;
+    int stop = 0;
+    int ask;
+    enum clnt_stat stat ;
+    static int res;
 
-  stat = callrpc(/* host */ host,
-		 /* prognum */ PROGNUM,
-		 /* versnum */ VERSNUM,
-		 /* procnum */ PROCNUM,
-		 /* argument encoding filter */ (xdrproc_t) xdr_int,
-		 /* argument */ (char *)&n,
-		 /* return value decoding filter */ (xdrproc_t)xdr_int,
-		 /* retour value */(char *)&res) ;
+    while(stop != 1)
+    {
+        printf(">");
+        scanf("%d",&ask);
+        switch(ask)
+        {
+            case 0:
+                stat = callrpc("localhost",
+                    PROGNUM, VERSNUM, PROCNUM,
+                    (xdrproc_t)xdr_void, (void *)0,
+                    (xdrproc_t)xdr_int, (char *)&res) ;
 
-  if (stat != RPC_SUCCESS)
-  {
-    fprintf(stderr, "Echec de l'appel distant\n") ;
-    clnt_perrno(stat) ;
-    fprintf(stderr, "\n") ;
-    return 1 ;
-  }
+                if (stat != RPC_SUCCESS)
+                {
+                    fprintf(stderr, "Echec de l'appel distant\n") ;
+                    clnt_perrno(stat) ;
+                    fprintf(stderr, "\n") ;
+                    //return 1 ;
+                }
+                break;
+            case 1:
+                stop = 1;
+                break;
+            default:
+                break;
+        }
+        printf("\n");
+    }
+    
+    pthread_exit(NULL);
+}
 
-  printf("client: variable n (end) : %d,\n",n) ;
-  printf("client: variable res : %d\n",res) ;
-  
-  return 0 ;
+int main(void)
+{
+    pthread_t thread_client;
+    
+    if(pthread_create(&thread_client, NULL, node, NULL) == -1){
+        perror("pthread_create");
+        return EXIT_FAILURE;
+    }
+
+    if(registerrpc(PROGNUM, VERSNUM, PROCNUM, hello, (xdrproc_t)xdr_void, (xdrproc_t)xdr_int) == -1){
+        fprintf(stderr, "unable to register 'hello' !\n");
+        return EXIT_FAILURE;
+    }
+
+    printf("thread launched\n");
+
+    svc_run();
+
+    if(pthread_join(thread_client,NULL)==-1){
+        perror("pthread_join");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
