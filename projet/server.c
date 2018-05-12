9aedc90119ac05fbe809e4550b869984629fc127
MorganeRitter <morganeritter@hotmail.fr>
2018-05-12 23:45:48 +0200

    begin blocknode

--- projet/server.c
+++ projet/server.c
@@ -8,25 +8,56 @@
 #define VERSNUM 1
 #define PROCNUM 1
 
+struct block 
+{
+
+};
+
+struct block_node
+{
+
+};
+
+struct part_node
+{
+
+};
+
+struct server
+{
+	
+};
+
+
 int * proc_dist(int *n)
 { 
-  static int res = 1 ;
-  printf("server: variable n (debut) : %d,\n",*n) ;
-  res = (*n) + 1 ;
-  *n = *n + 1 ;
-  printf("server: variable n (fin) : %d,\n",*n) ;
-  printf("server: variable res : %d\n",res) ;
-  return &res ;
+  	static int res = 1 ;
+  	printf("server: variable n (debut) : %d,\n",*n) ;
+  	res = (*n) + 1 ;
+  	*n = *n + 1 ;
+  	printf("server: variable n (fin) : %d,\n",*n) ;
+  	printf("server: variable res : %d\n",res) ;
+  	return &res ;
 }
+
+int create_block(int * nb, int * p)
+{
+	
+	
+	return 1;	
+}
+
+
 
 int main (void)
 {
   registerrpc(/* prognum */ PROGNUM,
 	     /* versnum */ VERSNUM,
 	     /* procnum */ PROCNUM,
-	     /* pointer on function */ proc_dist,
+	     /* pointer on function */ create_block,
 	     /* argument decoding */ (xdrproc_t)xdr_int,
 	     /* function result encoding */ (xdrproc_t)xdr_int) ;
+
 
   svc_run() ; /* server ready for clients */
 
