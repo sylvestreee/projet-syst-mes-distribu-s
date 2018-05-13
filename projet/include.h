0daf991661a82499f41ccad9562ae740be468331
MorganeRitter <morganeritter@hotmail.fr>
2018-05-13 14:40:56 +0200

    begin blocknode and xdr_filter

--- projet/include.h
+++ projet/include.h
@@ -1,22 +1,33 @@
+#ifndef __INCLUDE_H__
+#define __INCLUDE_H__
+
+#define NB 10
+#define LGTH 250
+
 struct block {
 	int depth;
 	int creator;	
 };
 
+typedef struct block block;
+
+typedef char request[LGTH];
+
 struct block_node {
 	int num;
-	int block_node_connect[10];
-	struct block b[10];
-	char * requests[10];
+	int block_node_connect[NB];
+	block b[NB];
+	request requests[NB];
 };
 
-typedef struct block block;
 typedef struct node_block node_block;
 
 block * malloc_block(void);
 
 block_node * malloc_block_node(void);
 
-bool_t xdr_block(XDR *xdrs, block * b);
+bool_t xdr_block(XDR *xdrs, block * block);
 
 bool_t xdr_block_node(XDR * xdrs, block_node * bn);
+
+#endif /* __INCLUDE_H__ */
--- projet/xdr_struct.c
+++ projet/xdr_struct.c
@@ -1,6 +1,6 @@
 #include "include.h"
 
-block * malloc_block(void)
+/*block * malloc_block(void)
 {
 	block * b;
 	b = (block *) malloc(sizeof(block));
@@ -18,11 +18,11 @@ void xdr_freep(void *p)
 {
   fprintf(stdout, "Zone désallouée : %x\n", (unsigned)p);
   free(p);
-}
+}*/
 
-bool_t xdr_block(XDR *xdrs, block ** block)
+bool_t xdr_block(XDR *xdrs, block * block)
 {
-	block * b;
+	/*block * b;
 	int n,c;
 	switch (xdrs->x_op)
 	{
@@ -61,48 +61,21 @@ bool_t xdr_block(XDR *xdrs, block ** block)
 		*block = b;
 	}
 	return TRUE;
+	*/
+	return (xdr_int(xdrs, &block->depth) && 
+			xdr_int(xdrs, &block->creator);
+}
+
+bool_t xdr_request(XDR * xdrs, char * request)
+{
+	return (xdr_string(xdrs, &request, LGTH));
 }
 
 bool_t xdr_block_node(XDR * xdrs, block_node * bn)
 {
-	block_node * b;
-	int n,c;
-	switch (xdrs->x_op)
-	{
-		case XDR_ENCODE : 
-			fprintf(stderr,"xdr_mat  : ENCODE(%d)\n", xdrs->x_op);   
-			break; 
-	    case XDR_DECODE : 
-			fprintf(stderr,"xdr_mat  : DECODE(%d)\n", xdrs->x_op);
-			break; 
-	    case XDR_FREE :
-			fprintf(stderr,"xdr_mat  : FREE(%d)\n", xdrs->x_op);
-			xdr_freep(b);
-			break; 
-	    default :
-			fprintf(stderr,"xdr_mat  : default(%d)\n", xdrs->x_op);
-			break; 
-	}
-
-	if(xdrs->op == XDR_ENCODE)
-	{
-		b = *bn;
-		n = bn -> num;
-		if(!xdr_int(xdrs, &n))
-      		return FALSE;
-		c = bn -> creator;
-		if(!xdr_u_int(xdrs, &c))
-		    return FALSE;
-	}
-	else if(xdrs->op == XDR_DECODE)
-	{
-		if(!xdr_int(xdrs, &n))
-			return FALSE;
-		if(!xdr_int(xdrs, &c))
-			return FALSE;
-		b = malloc_block(void);
-		*bn = b;
-	}
-	return TRUE;	
+	return (xdr_int(xdrs, &bn->num) &&
+			xdr_vector(xdrs, &bn->block_node_connect, NB, sizeof(int), xdr_int) &&
+			xdr_vector(xdrs, &bn->b, NB, sizeof(block), xdr_block) &&
+			xdr_vector(xdrs, &bn->requests, sizeof(request), xdr_request)
 }
 
