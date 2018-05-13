7ceb4a3bf47ffa013592c845c6db9c88a4c44eb2
MorganeRitter <morganeritter@hotmail.fr>
2018-05-13 17:22:35 +0200

    Try fixe xdr filter

--- projet/xdr_struct.c
+++ projet/xdr_struct.c
@@ -1,9 +1,13 @@
+#include <rpc/types.h>
+#include <rpc/xdr.h>
+#include <rpc/rpc.h>
+
 #include "include.h"
 
 bool_t xdr_block(XDR *xdrs, block * block)
 {
 	return (xdr_int(xdrs, &block->depth) && 
-			xdr_int(xdrs, &block->creator);
+			xdr_int(xdrs, &block->creator));
 }
 
 bool_t xdr_request(XDR * xdrs, char * request)
@@ -14,8 +18,8 @@ bool_t xdr_request(XDR * xdrs, char * request)
 bool_t xdr_block_node(XDR * xdrs, block_node * bn)
 {
 	return (xdr_int(xdrs, &bn->num) &&
-			xdr_vector(xdrs, &bn->block_node_connect, NB, sizeof(int), xdr_int) &&
+			xdr_vector(xdrs,(char *) &bn->block_node_connect, NB, sizeof(int), xdr_int) &&
 			xdr_vector(xdrs, &bn->b, NB, sizeof(block), xdr_block) &&
-			xdr_vector(xdrs, &bn->requests, sizeof(request), xdr_request)
+			xdr_vector(xdrs, &bn->requests, sizeof(request), xdr_request));
 }
 
