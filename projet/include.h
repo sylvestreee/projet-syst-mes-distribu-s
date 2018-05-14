4c77ded1b37938ab39b8bc7df628e85455acadd0
MorganeRitter <morganeritter@hotmail.fr>
2018-05-14 12:27:42 +0200

    Fix filter xdr

--- projet/include.h
+++ projet/include.h
@@ -1,42 +1,72 @@
 #ifndef __INCLUDE_H__
 #define __INCLUDE_H__
 
-#define NB 10
-#define LGTH 250
+#include <rpc/rpc.h>
 
-typedef char request[LGTH];
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-struct block 
-{
+
+struct request {
+	int sender;
+	char * entitle;
+	int receiver;
+};
+typedef struct request request;
+
+
+struct block {
 	int depth;
 	int creator;
-	// hash
-	request requests[NB];
+	request requests[10];
 };
-
 typedef struct block block;
+
 
 struct participant_node {
 	int value;
-	int block_node_connect[NB]; 
+	int block_node_connect[10];
 };
-
 typedef struct participant_node participant_node;
+
 
 struct block_node {
 	int num;
-	int block_node_connect[NB];
-	block * b[NB]; // blockchain
-	//participant_node p[NB]; // participant_node which are connected to the blocknode
-	request requests[NB];
+	int block_node_connect[10];
+	block b[10];
+	participant_node p[10];
+	request requests[10];
 };
-
 typedef struct block_node block_node;
 
-bool_t xdr_request(XDR * xdrs, char * request);
 
-bool_t xdr_block(XDR *xdrs, block * block);
+/* the xdr functions */
 
-bool_t xdr_block_node(XDR * xdrs, block_node * bn);
+#if defined(__STDC__) || defined(__cplusplus)
+extern  bool_t xdr_request (XDR *, request*);
+extern  bool_t xdr_request (XDR *, request*);
+extern  bool_t xdr_block (XDR *, block*);
+extern  bool_t xdr_block (XDR *, block*);
+extern  bool_t xdr_participant_node (XDR *, participant_node*);
+extern  bool_t xdr_participant_node (XDR *, participant_node*);
+extern  bool_t xdr_block_node (XDR *, block_node*);
+extern  bool_t xdr_block_node (XDR *, block_node*);
+
+#else /* K&R C */
+extern bool_t xdr_request ();
+extern bool_t xdr_request ();
+extern bool_t xdr_block ();
+extern bool_t xdr_block ();
+extern bool_t xdr_participant_node ();
+extern bool_t xdr_participant_node ();
+extern bool_t xdr_block_node ();
+extern bool_t xdr_block_node ();
+
+#endif /* K&R C */
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* __INCLUDE_H__ */
